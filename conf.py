0a136f87b82b9efd602c17a33e3239b9b55cdef4
Alex Caudill <hypoalex@users.noreply.github.com>
2016-03-31 02:50:55 -0500

    Add trailing newline for flake8

--- conf.py
+++ conf.py
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-#
-# -- General configuration -----------------------------------------------------
+# -- General configuration ----------------------------------------------------
 
 source_suffix = '.rst'
 master_doc = 'index'
@@ -8,4 +6,4 @@ master_doc = 'index'
 project = u'Passer: A Python implementation of the Sparrow task scheduler'
 copyright = u'2016 Alex Caudill'
 
-version = '0.0.1'
+version = '0.0.2'
